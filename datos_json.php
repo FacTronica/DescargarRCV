3a1c7788b533fd70924f6b2671851424c4743d3e
FacTrónica <webmaster@factronica.cl>
2023-04-26 23:26:42 -0400

    Create datos_json.php

---
+++ datos_json.php
@@ -0,0 +1,56 @@
+$arregloJson = array(
+
+ 
+"TOKEN" => "apitest",
+
+# RUT EMPRESA
+"rutEmisor"=>"11111111",
+
+
+# DV RUT EMPRESA
+"dvEmisor"=>"1",
+
+# PERIODO AÑO MES
+"PeriodoTributario" => "202208",
+
+# REGISTRO ( CONSTANTE )
+"Operacion"=>"REGISTRO",
+
+# REGISTRO A DESCARGAR getDetalleCompraExport O getDetalleVentaExport
+"Origen"=>"getDetalleCompraExport",
+
+# 1=produccion 0=certificacion
+"PRODUCCION"=>"1",
+
+
+# Nombre del archivo de salida para guardar el resultado
+"ARCHIVOCSV"=>"DETALLE_RCV_202208_COMPRA.csv", 
+ 
+
+#########################################
+# CERTIFICADO DIGITAL
+#########################################
+"Modulus"=>"hWE5X27A9DM41E7gThwTpFlAANWKVSDpamCkADwVEur+tR1nsW6djhVGJalOd6Qn
+/r8zoF0l5lQY1ZqAnfZvMrAblLNNist4JZ19rfCa4cXLg/iu1zbBaz9VEwNukov5
+lrc8hsWwJvtcrplzNMBQJw==",
+
+"Exponent"=>"AQAB",
+
+"X509Certificate"=>"MIIGbzCCBVegAwIBAgIIWuTtr5RiRFAwDQYJKoZIhvcNAQELBQAwga8xCzAJBgNV
+BAYTAkNMMRQwEgYDVQQKDAtFLVNpZ24gUy5BLjE5MDcGA1UECwwwVGVybXMgb2Yg
+dXNlIGF0IHd3dy5lc2lnbi1sYS5jb20vYWN1ZXJkb3RlcmNlcm9zMSswKQYDVQQD
+9teRsxdg1O3a0CWryvBEw/y4eqgJLdEoZ1uiKH8dRjrna4wNb/xWY0c1TfotxJZ2
+6d9eGovIrrYbZoHRt8dHvOzHuFyQxNAYVVmEW7oLeXz72LxzseiEo0exbk1gebJq
+u9JpvJ25E/J6XObgcTLFRBJFT/myFTs334MH4xPzXfr4nyaFEB8h5cLz5c0poZTK
+WTKagXFjCX5iRwqXJM1v54IYG5fZk0Grs1qPDMfD4xQLD39KHBrqdgLBmONhDwSy
+aCuVhhl6jLLp0Jrddw0PWtQ5XQ==",
+
+"PrivKey"=>"-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAhWE5X27A9DM41E7gThwTpFlAANWKVSDpamCkADwVEur+tR1n
+sW6djhVGJalOd6Qnv5TCOX40T6N9UDCfF/XsOQHUqP/C9wT3ftwQOSGDZlc4TblS
+L/0tSH0CgYBp/2S3g96WBUrqJRWnX5NhB5urROj+jtNNqBRC6L2g8rg0OapDPgaJ
+4IQjitVL/+Bqcw14IZqbdpVveB1wy5QZPUXeeMoUvusuQyvvageU503f/LI21RnO
+13feTCoC2ImcYsMmi9fEQgEHjZJZdXmYmJrCwr/Y7pdebQQ9IL3eCQ==
+-----END RSA PRIVATE KEY-----" 
+
+);
